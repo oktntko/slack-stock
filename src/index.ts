c0c3fba90bf32d30f0b112022fd84d31bb79063e
oktntko <oktntko@gmail.com>
2023-12-26 23:20:04 +0900

    linter & formatter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { Command } from "@commander-js/extra-typings";
+import { Command } from '@commander-js/extra-typings';
 
 const program = new Command()
-  .argument("<string>")
-  .option("--first")
-  .option("-s, --separator <char>")
+  .argument('<string>')
+  .option('--first')
+  .option('-s, --separator <char>')
   .action((str, options) => {
     const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator ?? "/", limit));
+    console.log(str.split(options.separator ?? '/', limit));
   });
 
 program.parse();
