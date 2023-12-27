3f6da361ff64febbfe61c5e159392c7409a79369
oktntko <oktntko@gmail.com>
2023-12-27 20:48:52 +0900

    password

---
+++ src/repository/PasswordRepository.ts
@@ -0,0 +1,43 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+const PASSWORD_LENGTH = 23;
+
+const PASSWORD_PATH = path.join(os.homedir(), '.slack-stock.password');
+
+if (!fs.existsSync(PASSWORD_PATH)) {
+  fs.writeFileSync(PASSWORD_PATH, crypto.randomBytes(PASSWORD_LENGTH).toString('base64'), 'utf8');
+}
+
+const PASSWORD = fs.readFileSync(PASSWORD_PATH, 'utf8');
+
+////////////////////////////////
+// 暗号化・複合化
+////////////////////////////////
+export const PasswordRepository = {
+  encrypt,
+  decrypt,
+};
+
+const ALGORITHM = 'aes-256-ctr';
+const IV_LENGTH = 16;
+
+function encrypt(text: string, key = PASSWORD) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+
+  return Buffer.concat([iv, cipher.update(Buffer.from(text)), cipher.final()]).toString('base64');
+}
+
+function decrypt(data: string, key = PASSWORD) {
+  const buff = Buffer.from(data, 'base64');
+
+  const iv = buff.subarray(0, IV_LENGTH);
+  const encData = buff.subarray(IV_LENGTH);
+
+  const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+
+  return Buffer.concat([decipher.update(encData), decipher.final()]).toString('utf8');
+}
