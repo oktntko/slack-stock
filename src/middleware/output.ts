943958930f930e610a30a4e81510c0f714404295
oktntko <oktntko@gmail.com>
2023-12-29 01:15:42 +0900

    nested object output file

--- src/middleware/output.ts
+++ src/middleware/output.ts
@@ -5,24 +5,24 @@ import xlsx from 'xlsx';
 
 export async function output(
   outputFormat: 'console' | 'csv' | 'xlsx',
-  values: object[],
+  records: object[],
   filePath: string,
 ) {
   switch (outputFormat) {
     case 'console': {
-      console.table(values);
-      return;
+      console.table(records);
+      return filePath;
     }
     default: {
-      writeFile(outputFormat, values, filePath!);
-      return;
+      writeFile(outputFormat, records, filePath);
+      return filePath;
     }
   }
 }
 
-function writeFile(bookType: 'csv' | 'xlsx', values: object[], filePath: string) {
-  // TODO: nested property
-  const data = [Object.keys(values[0]), ...values.map((v) => Object.values(v))];
+function writeFile(bookType: 'csv' | 'xlsx', records: object[], filePath: string) {
+  const flattenRecords = records.map((x) => flatten(x));
+  const data = [Object.keys(flattenRecords[0]), ...flattenRecords.map((x) => Object.values(x))];
   const workbook = xlsx.utils.book_new();
   const worksheet = xlsx.utils.aoa_to_sheet(data);
 
@@ -64,4 +64,40 @@ export function filepath(
       return basepath(`@slst_${dataType}_${dayjs().format('YYYY-MM-DD')}.${outputFormat}`);
     }
   }
+}
+
+// https://stackoverflow.com/questions/33036487/one-liner-to-flatten-nested-object
+// $roots keeps previous parent properties as they will be added as a prefix for each prop.
+// $sep is just a preference if you want to seperate nested paths other than dot.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+function flatten<T extends object>(obj: T, roots = [], sep = '.') {
+  return (
+    Object
+      // find props of given object
+      .keys(obj)
+      // return an object by iterating props
+      .reduce(
+        (memo, prop) => {
+          return Object.assign(
+            // create a new object
+            {},
+            // include previously returned object
+            memo,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            Object.prototype.toString.call(obj[prop]) === '[object Object]'
+              ? // keep working if value is an object
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                flatten(obj[prop], roots.concat([prop]), sep)
+              : // include current prop and value and prefix prop with the roots
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                { [roots.concat([prop]).join(sep)]: obj[prop] },
+          );
+        },
+        {} as Record<string, string>,
+      )
+  );
 }
